5da44ee76ddce1a128bfda13dfc1fb595bc5072d
cold_hailstone <cksn1993@gmail.com>
2023-12-13 14:56:38 +0900

    5.3 Realm SDK

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useCallback, useEffect, useState } from 'react';
+import Realm from 'realm';
 import Navigator from './navigator';
 
+const FeelingSchema = {
+    name: 'Feeling',
+    properties: {
+        _id: 'int',
+        emotion: 'string',
+        message: 'string',
+    },
+    primaryKey: '_id',
+};
+
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+    const [ready, setReady] = useState(false);
+    const startLoading = async () => {
+        const realm = await Realm.open({
+            path: 'nomadDiaryDB',
+            schema: [FeelingSchema],
+        });
+    };
+    useEffect(() => {
+        startLoading();
+    }, []);
+
+    const onLayoutRootView = useCallback(async () => {
+        if (ready) {
+            await SplashScreen.hideAsync();
+        }
+    }, [ready]);
+
     return (
-        <NavigationContainer>
+        <NavigationContainer onReady={onLayoutRootView}>
             <Navigator />
         </NavigationContainer>
     );
